f94c81f264a6174633e973cdb3ebd042392fb823
dinah-parag <dinah.dantass@gmail.com>
2025-08-19 21:07:14 -0300

    newClasses

---
+++ src/brcomdiodesafio/brcomdiodesafiodominio/Main.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+    Curso curso1 = new Curso();
+    curso1.setTitulo("Curso Java");
+    curso1.setDescricao("Descrição do curso Java");
+    curso1.setCargaHoraria(8);
+
+    Curso curso2 = new Curso();
+    curso2.setTitulo("Curso Java Script");
+    curso2.setDescricao("Descrição do curso Java Script");
+    curso2.setCargaHoraria(4);
+
+    Mentoria mentoria1 = new Mentoria();
+    mentoria1.setTitulo("Mentoria de Java");
+    mentoria1.setDescricao("Descrição mentoria Java");
+    mentoria1.setData(LocalDate.now());
+
+    System.out.println(mentoria1);
+    }
+}
