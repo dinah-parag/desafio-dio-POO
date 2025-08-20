ac409de28d7c98275936f5a40594d2ce97031099
dinah-parag <dinah.dantass@gmail.com>
2025-08-20 14:26:46 -0300

    addRel

--- src/brcomdiodesafio/brcomdiodesafiodominio/Dev.java
+++ src/brcomdiodesafio/brcomdiodesafiodominio/Dev.java
@@ -1,4 +1,5 @@
 import java.util.LinkedHashSet;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -6,11 +7,24 @@ public class Dev {
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
-    public void increverBootcamp(Bootcamp bootcamp) {}
+    public void increverBootcamp(Bootcamp bootcamp) {
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
 
-    public void progredir() {}
+    }
+    public void progredir() {
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()) {
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+        System.err.println("Você não está matriculado em nenhum Conteúdo!");
+        }
+    }
 
-    public void calcularTotalXP() {}
+    public double calcularTotalXP() {
+        return this.conteudosConcluidos.stream().mapToDouble(conteudo -> conteudo.calcularXP()).sum();
+    }
 
     public String getNome() {
         return nome;
